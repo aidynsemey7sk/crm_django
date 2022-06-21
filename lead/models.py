000c4377592ce5c0e42c838491792d27c1c519ed
aidyn <aidynsemey7sk@gmail.com>
2022-06-21 15:32:35 +0600

    lead

--- lead/models.py
+++ lead/models.py
@@ -40,3 +40,6 @@ class Lead(models.Model):
     created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.contact_person
